9d3bab26b3e83c10a85b46887f3f173c40f10a4d
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-07-27 01:09:10 +0200

    Add docstrings to generated python functions, in lieu of typing generically.

--- examples/1/example1.py
+++ examples/1/example1.py
@@ -1,8 +1,17 @@
 import numpy as np
 
+
 def copy_3_times() -> np.ndarray:
+    """
+    <Docstring TODO!>
+
+    Returns: Int32[?]
+    """
     pass
+
 
 def main():
+    """
+    <Docstring TODO!>
+    """
     pass
-
--- src/languages/python.rs
+++ src/languages/python.rs
@@ -7,37 +7,67 @@ pub struct PythonTranspiler {
 
 }
 
+struct TypeInformation {
+    python_type: String,
+    docstring_type: String
+}
+
 impl PythonTranspiler {
     pub fn transpile(&self, program: &Program, stream: &mut (dyn Write)) -> Result<(), std::io::Error> {
-        write!(stream, "import numpy as np\n\n")?;
+        writeln!(stream, "import numpy as np")?;
 
         for function in program.functions.iter() {
-            write!(stream, "def {}()", function.identifier)?;
-            match &function.return_type {
-                Some(t) => write!(stream, " -> {}", self.transpile_type(&t))?,
-                _ => {}
-            }
-            write!(stream, ":\n    pass\n\n")?;
+            write!(stream, "\n\ndef {}()", function.identifier)?;
+            let return_info = function.return_type.as_ref().map(|t| self.transpile_type(&t));
 
+            if let Some(return_info) = &return_info {
+                write!(stream, " -> {}", return_info.python_type)?;
+            }
+
+            write!(stream, ":\n    \"\"\"\n    <Docstring TODO!>\n")?;
+
+            if let Some(return_info) = &return_info {
+                write!(stream, "\n    Returns: {}\n", return_info.docstring_type)?;
+            }
+
+            write!(stream, "    \"\"\"\n    pass\n")?;
         }
 
         return Ok(())
     }
 
-    pub fn transpile_type(&self, type_def: &Type) -> String {
+    pub fn transpile_type(&self, type_def: &Type) -> TypeInformation {
         match type_def.borrow() {
             Type::Identifier(t) => {
                 match t.as_str() {
-                    "Int32" => String::from("np.int32"),
-                    "Int64" => String::from("np.int64"),
-                    "Float32" => String::from("np.float32"),
-                    "Float64" => String::from("np.float64"),
-                    _ => t.clone()
+                    "Int32" => TypeInformation {
+                        python_type: String::from("np.int32"),
+                        docstring_type: String::from("int32")
+                    },
+                    "Int64" => TypeInformation {
+                        python_type: String::from("np.int64"),
+                        docstring_type: String::from("int64")
+                    },
+                    "Float32" => TypeInformation {
+                        python_type: String::from("np.float32"),
+                        docstring_type: String::from("float32")
+                    },
+                    "Float64" => TypeInformation {
+                        python_type: String::from("np.float64"),
+                        docstring_type: String::from("float64")
+                    },
+                    _ => TypeInformation {
+                        python_type: t.clone(),
+                        docstring_type: t.clone()
+                    }
                 }
             },
             Type::NDArray(atom) => {
                 match atom.as_ref() {
-                    Type::Identifier(_) => format!("np.ndarray"),
+                    Type::Identifier(atom) => TypeInformation {
+                        python_type: String::from("np.ndarray"),
+                        docstring_type: String::from(format!("{}[?]", atom))
+                    },
                     Type::NDArray(_) => panic!("Numpy does not support nested ndarrays.")
                 }
             }
