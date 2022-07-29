89ebe73af629f02a0280ffc6e1ca822e1cf93e80
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-07-30 00:07:03 +0200

    Move linker type parsing to its own file.

--- src/linker.rs
+++ src/linker.rs
@@ -1,8 +1,8 @@
 pub mod builtins;
 pub mod computation_tree;
 pub mod scopes;
+pub mod types;
 
-use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::rc::Rc;
 use guard::guard;
@@ -12,6 +12,7 @@ use crate::abstract_syntax;
 use crate::linker::builtins::*;
 use crate::linker::computation_tree::*;
 use crate::linker::scopes::*;
+use crate::linker::types::*;
 
 
 pub fn resolve_program(syntax: abstract_syntax::Program) -> Program {
@@ -340,33 +341,6 @@ pub fn resolve_expression(syntax: &abstract_syntax::Expression, variables: &Scop
         }
         abstract_syntax::Expression::MemberLookup(_, _) => {
             todo!()
-        }
-    })
-}
-
-pub fn resolve_type(syntax: &abstract_syntax::TypeDeclaration) -> Box<Type> {
-    Box::new(match syntax.borrow() {
-        abstract_syntax::TypeDeclaration::Identifier(id) => {
-            use PrimitiveType::*;
-            match id.as_str() {
-                "Bool" => Type::Primitive(Bool),
-                "Int8" => Type::Primitive(Int8),
-                "Int16" => Type::Primitive(Int16),
-                "Int32" => Type::Primitive(Int32),
-                "Int64" => Type::Primitive(Int64),
-                "Int128" => Type::Primitive(Int128),
-                "UInt8" => Type::Primitive(UInt8),
-                "UInt16" => Type::Primitive(UInt16),
-                "UInt32" => Type::Primitive(UInt32),
-                "UInt64" => Type::Primitive(UInt64),
-                "UInt128" => Type::Primitive(UInt128),
-                "Float32" => Type::Primitive(Float32),
-                "Float64" => Type::Primitive(Float64),
-                _ => Type::Identifier(id.clone())
-            }
-        },
-        abstract_syntax::TypeDeclaration::NDArray(identifier, _) => {
-            Type::NDArray(resolve_type(&identifier))
         }
     })
 }
