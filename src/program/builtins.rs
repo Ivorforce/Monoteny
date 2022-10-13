986d081466ba0f9e377661950c2f60fa9fb4808f
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-10-14 00:09:26 +0200

    Optimize builtins imports.

--- src/program/builtins.rs
+++ src/program/builtins.rs
@@ -1,19 +1,6 @@
-use std::collections::{HashMap, HashSet};
 use std::rc::Rc;
-use itertools::zip_eq;
-use uuid::Uuid;
-use precedence::PrecedenceGroups;
-use primitives::Primitives;
-use traits::Traits;
-use crate::program::traits::{Trait, TraitConformanceDeclaration, TraitConformanceRequirement};
-use crate::parser;
-use crate::linker::precedence::{OperatorAssociativity, PrecedenceGroup};
+
 use crate::linker::scopes::Scope;
-use crate::program::types::*;
-use crate::program;
-use crate::program::allocation::Reference;
-use crate::program::functions::{FunctionForm, FunctionPointer, HumanFunctionInterface, MachineFunctionInterface};
-use crate::program::structs::Struct;
 
 pub mod precedence;
 pub mod debug;
@@ -22,11 +9,11 @@ pub mod traits;
 pub mod primitives;
 
 pub struct Builtins {
-    pub traits: Traits,
+    pub traits: traits::Traits,
     pub debug: debug::Functions,
-    pub primitives: Primitives,
+    pub primitives: primitives::Primitives,
     pub strings: strings::Strings,
-    pub precedence_groups: PrecedenceGroups,
+    pub precedence_groups: precedence::PrecedenceGroups,
 
     pub global_constants: Scope<'static>,
 }
