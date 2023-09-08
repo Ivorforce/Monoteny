0521c4ec149358ca25a2f17212860c7831bedbb2
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2023-09-08 13:58:54 +0200

    Add a comment that locals are wrongly implemented with the current interpreter. This is ok for now as nested function calls are rare-ish and thus not a priority.

--- src/interpreter/compiler.rs
+++ src/interpreter/compiler.rs
@@ -17,6 +17,7 @@ pub fn compile_function(function: &Rc<FunctionImplementation>) -> FunctionInterp
                 implementation: f,
                 requirements_fulfillment: FunctionInterpreter::combine_bindings(&interpreter.requirements_fulfillment, binding),
                 globals: interpreter.globals,
+                // TODO The locals should be on the stack!! For now it's impossible to run the same function in a nested way.
                 locals: HashMap::new(),
             };
             sub_interpreter.assign_arguments(arguments);
