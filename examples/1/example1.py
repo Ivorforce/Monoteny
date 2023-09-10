c0b7b5469357997add1c34a4a6e18916808dd118
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2023-09-11 00:11:50 +0200

    Actually use floor() for floor_div in example1.

--- examples/1/example1.py
+++ examples/1/example1.py
@@ -11,15 +11,35 @@ def main():
     """
     print("Test: \"Success\"")
     value = square_1(float32(2.2)) * math.sin(e())
-    b = square_0(value=value) + pi_ish_0()
+    b = square_0(value=value) + pi_ish_1()
     b = floor_div(square_1(b) ** (-float32(2.2)), value) + tau()
-    c = pi_ish_1() // int32(2)
+    c = pi_ish_0() // int32(2)
     print(((b < float32(2)) and (not (value > float32(2)))) and True)
 
 
 # ========================== ======== ============================
 # ========================== Internal ============================
 # ========================== ======== ============================
+
+
+def e() -> float32:
+    """
+    <DOCSTRING TODO>
+
+    Returns:
+        <TODO>
+    """
+    return float32(2.718281828459045)
+
+
+def pi_ish_0() -> int32:
+    """
+    <DOCSTRING TODO>
+
+    Returns:
+        <TODO>
+    """
+    return int32(3)
 
 
 def square_0(value: float32) -> float32:
@@ -46,17 +66,17 @@ def floor_div(lhs: float32, rhs: float32) -> float32:
     Returns:
         <TODO>
     """
-    return lhs / rhs
+    return math.floor(lhs / rhs)
 
 
-def e() -> float32:
+def pi_ish_1() -> float32:
     """
     <DOCSTRING TODO>
 
     Returns:
         <TODO>
     """
-    return float32(2.718281828459045)
+    return float32(3.14)
 
 
 def tau() -> float32:
@@ -67,16 +87,6 @@ def tau() -> float32:
         <TODO>
     """
     return float32(6.283185307179586)
-
-
-def pi_ish_0() -> float32:
-    """
-    <DOCSTRING TODO>
-
-    Returns:
-        <TODO>
-    """
-    return float32(3.14)
 
 
 def square_1(self: float32) -> float32:
@@ -90,16 +100,6 @@ def square_1(self: float32) -> float32:
         <TODO>
     """
     return self * self
-
-
-def pi_ish_1() -> int32:
-    """
-    <DOCSTRING TODO>
-
-    Returns:
-        <TODO>
-    """
-    return int32(3)
 
 
 __all__ = [
