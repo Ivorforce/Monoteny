0458ea054ff4a4d959169847568da53f3f50c508
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2023-09-10 14:00:58 +0200

    Unescape every character captured with \ in strings - we can still unescape other sequences (like swift's \()) in the future.

--- src/parser/strings.rs
+++ src/parser/strings.rs
@@ -2,5 +2,5 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
-    pub static ref REGEX_UNESCAPE: Regex = Regex::new(r"\\").unwrap();
+    pub static ref REGEX_UNESCAPE: Regex = Regex::new(r"\\(.)").unwrap();
 }
