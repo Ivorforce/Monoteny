59f997df368fc5705be5737bd7309c28544f7ff1
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2023-09-29 19:36:17 +0200

    Fully decouple python code from transpiler code: This should make it easier to implement other language transpilers, as well as construct transpilers within the language, later.

--- src/transpiler.rs
+++ src/transpiler.rs
@@ -1,2 +1,109 @@
+use std::cell::RefCell;
+use std::collections::HashSet;
+use std::ops::DerefMut;
+use std::rc::Rc;
+use guard::guard;
+use itertools::Itertools;
+use crate::constant_folding::ConstantFold;
+use crate::interpreter;
+use crate::interpreter::{InterpreterError, Runtime};
+use crate::monomorphize::Monomorphizer;
+use crate::program::calls::FunctionBinding;
+use crate::program::functions::FunctionHead;
+use crate::program::global::FunctionImplementation;
+use crate::program::module::Module;
+use crate::program::traits::RequirementsFulfillment;
+
 pub mod python;
 pub mod namespaces;
+
+pub struct Transpiler {
+    pub monomorphizer: Box<Monomorphizer>,
+    pub exported_functions: Vec<Box<FunctionImplementation>>,
+    pub internal_functions: Vec<Box<FunctionImplementation>>,
+}
+
+pub struct Context {
+    pub builtin_functions: HashSet<Rc<FunctionHead>>,
+}
+
+pub fn run(module: &Module, runtime: &mut Runtime, context: Context) -> Result<Transpiler, InterpreterError> {
+    let transpiler = Transpiler {
+        monomorphizer: Box::new(Monomorphizer::new()),
+        exported_functions: vec![],
+        internal_functions: vec![],
+    };
+
+    let transpiler = Rc::new(RefCell::new(transpiler));
+
+    interpreter::run::transpile(module, runtime, &|implementation_id, runtime| {
+        let mut transpiler = transpiler.borrow_mut();
+        let transpiler_context = transpiler.deref_mut();
+        let implementation = &runtime.source.fn_implementations[&implementation_id];
+
+        if implementation.head.interface.collect_generics().len() > 0 {
+            // We'll need to somehow transpile requirements as protocols and generics as generics.
+            // That's for later!
+            panic!("Transpiling generic functions is not supported yet: {:?}", implementation.head);
+        }
+
+        let mut mono_implementation = implementation.clone();
+        transpiler_context.monomorphizer.monomorphize_function(
+            &mut mono_implementation,
+            &Rc::new(FunctionBinding {
+                // The implementation's pointer is fine.
+                function: Rc::clone(&implementation.head),
+                // The resolution SHOULD be empty: The function is transpiled WITH its generics.
+                // Unless generics are bound in the transpile directive, which is TODO
+                requirements_fulfillment: RequirementsFulfillment::empty(),
+            }),
+            &|f| !context.builtin_functions.contains(&f.function)
+        );
+
+        transpiler_context.exported_functions.push(mono_implementation);
+    })?;
+
+    // Find and monomorphize internal symbols
+    let mut transpiler = Rc::try_unwrap(transpiler).map_err(|_| ()).expect("Internal Error on try_unwrap(transpiler)").into_inner();
+
+    while let Some(function_binding) = transpiler.monomorphizer.new_encountered_calls.pop() {
+        guard!(let Some(implementation) = runtime.source.fn_implementations.get(&function_binding.function) else {
+            // We don't have an implementation ready, so it must be a builtin or otherwise injected.
+            continue;
+        });
+
+        // We may not create a new one through monomorphization, but we still need to take ownership.
+        let mut mono_implementation = implementation.clone();
+        // If the call had an empty fulfillment, it wasn't monomorphized. We can just use the implementation itself!
+        if transpiler.monomorphizer.resolved_call_to_mono_call.contains_key(&function_binding) {
+            transpiler.monomorphizer.monomorphize_function(
+                &mut mono_implementation,
+                &function_binding,
+                &|f| !context.builtin_functions.contains(&f.function)
+            );
+        };
+
+        transpiler.internal_functions.push(mono_implementation);
+    }
+
+    Ok(transpiler)
+}
+
+pub fn constant_fold(transpiler: &mut Transpiler) {
+    // Run constant folder
+    let mut constant_folder = ConstantFold::new();
+    let exported_function_order = transpiler.exported_functions.iter().map(|x| Rc::clone(&x.head)).collect_vec();
+
+    // The exported functions aren't called so it makes sense to prepare the internal functions first.
+    for implementation in transpiler.internal_functions.drain(..) {
+        constant_folder.add(implementation, true);
+    }
+    for implementation in transpiler.exported_functions.drain(..) {
+        constant_folder.add(implementation, false);
+    }
+
+    // Exported functions MUST be there still, because they can't be inlined.
+    transpiler.exported_functions.extend(exported_function_order.iter().map(|x| constant_folder.implementation_by_head.remove(x).unwrap()).collect_vec());
+    // The order of the internal functions is unimportant anyway, because they are sorted later.
+    transpiler.internal_functions = constant_folder.drain_all_functions_yield_uninlined();
+}
--- src/transpiler/python.rs
+++ src/transpiler/python.rs
@@ -5,135 +5,50 @@ pub mod ast;
 pub mod imperative;
 pub mod representations;
 
-use std::cell::RefCell;
 use std::collections::HashSet;
 use std::collections::hash_map::Entry;
 use std::ops::DerefMut;
-use std::rc::Rc;
-use guard::guard;
 use itertools::Itertools;
 use uuid::Uuid;
-use crate::constant_folding::ConstantFold;
-use crate::monomorphize::Monomorphizer;
-use crate::interpreter;
+use crate::transpiler;
 use crate::interpreter::{Runtime, InterpreterError};
 
 use crate::program::computation_tree::*;
-use crate::program::calls::FunctionBinding;
-use crate::program::global::{BuiltinFunctionHint, FunctionImplementation};
+use crate::program::global::BuiltinFunctionHint;
 use crate::program::module::Module;
-use crate::program::traits::RequirementsFulfillment;
 use crate::program::types::TypeUnit;
-use crate::transpiler::namespaces;
+use crate::transpiler::{constant_fold, namespaces, Transpiler};
 use crate::transpiler::python::ast::Statement;
 use crate::transpiler::python::class::{ClassContext, transpile_class};
 use crate::transpiler::python::imperative::{FunctionContext, transpile_function};
 use crate::transpiler::python::representations::Representations;
 
 
-pub struct TranspilerContext {
-    monomorphizer: Box<Monomorphizer>,
-    exported_functions: Vec<Box<FunctionImplementation>>,
-    internal_functions: Vec<Box<FunctionImplementation>>,
-}
-
 pub fn transpile_module(module: &Module, runtime: &mut Runtime, should_constant_fold: bool) -> Result<Box<ast::Module>, InterpreterError> {
     let mut representations = Representations::new();
-    let builtin_level = builtins::register(runtime, &mut representations);
-
-    let transpiler_context = TranspilerContext {
-        monomorphizer: Box::new(Monomorphizer::new()),
-        exported_functions: vec![],
-        internal_functions: vec![],
-    };
-
-    let transpiler_context = Rc::new(RefCell::new(transpiler_context));
+    let builtin_namespace = builtins::register(runtime, &mut representations);
 
     // Run interpreter
-
-    interpreter::run::transpile(module, runtime, &|implementation_id, runtime| {
-        let mut transpiler_context = transpiler_context.borrow_mut();
-        let transpiler_context = transpiler_context.deref_mut();
-        let implementation = &runtime.source.fn_implementations[&implementation_id];
-
-        if implementation.head.interface.collect_generics().len() > 0 {
-            // We'll need to somehow transpile requirements as protocols and generics as generics.
-            // That's for later!
-            panic!("Transpiling generic functions is not supported yet: {:?}", implementation.head);
-        }
-
-        let mut mono_implementation = implementation.clone();
-        transpiler_context.monomorphizer.monomorphize_function(
-            &mut mono_implementation,
-            &Rc::new(FunctionBinding {
-                // The implementation's pointer is fine.
-                function: Rc::clone(&implementation.head),
-                // The resolution SHOULD be empty: The function is transpiled WITH its generics.
-                // Unless generics are bound in the transpile directive, which is TODO
-                requirements_fulfillment: RequirementsFulfillment::empty(),
-            }),
-            &|f| !representations.builtin_functions.contains(&f.function)
-        );
-
-        transpiler_context.exported_functions.push(mono_implementation);
+    let mut transpiler = transpiler::run(module, runtime, transpiler::Context {
+        builtin_functions: representations.builtin_functions.clone(),
     })?;
 
-    // Find and monomorphize internal symbols
-    let mut transpiler_context = transpiler_context.borrow_mut();
-    let mut transpiler_context = transpiler_context.deref_mut();
-
-    while let Some(function_binding) = transpiler_context.monomorphizer.new_encountered_calls.pop() {
-        guard!(let Some(implementation) = runtime.source.fn_implementations.get(&function_binding.function) else {
-            // We don't have an implementation ready, so it must be a builtin or otherwise injected.
-            continue;
-        });
-
-        // We may not create a new one through monomorphization, but we still need to take ownership.
-        let mut mono_implementation = implementation.clone();
-        // If the call had an empty fulfillment, it wasn't monomorphized. We can just use the implementation itself!
-        if transpiler_context.monomorphizer.resolved_call_to_mono_call.contains_key(&function_binding) {
-            transpiler_context.monomorphizer.monomorphize_function(
-                &mut mono_implementation,
-                &function_binding,
-                &|f| !representations.builtin_functions.contains(&f.function)
-            );
-        };
-
-        transpiler_context.internal_functions.push(mono_implementation);
-    }
-
     if should_constant_fold {
-        // Run constant folder
-        let mut constant_folder = ConstantFold::new();
-        let exported_function_order = transpiler_context.exported_functions.iter().map(|x| Rc::clone(&x.head)).collect_vec();
-
-        // The exported functions aren't called so it makes sense to prepare the internal functions first.
-        for implementation in transpiler_context.internal_functions.drain(..) {
-            constant_folder.add(implementation, true);
-        }
-        for implementation in transpiler_context.exported_functions.drain(..) {
-            constant_folder.add(implementation, false);
-        }
-
-        // Exported functions MUST be there still, because they can't be inlined.
-        transpiler_context.exported_functions.extend(exported_function_order.iter().map(|x| constant_folder.implementation_by_head.remove(x).unwrap()).collect_vec());
-        // The order of the internal functions is unimportant anyway, because they are sorted later.
-        transpiler_context.internal_functions = constant_folder.drain_all_functions_yield_uninlined();
+        constant_fold(&mut transpiler);
     }
 
-    // TODO We need to sort the internal functions. This could be done roughly by putting them in the
+    // TODO We should sort the internal functions. This could be done roughly by putting them in the
     //  order the player defined it - which leaves only different monomorpizations to be sorted.
     //  Those can be sorted by something like the displayed 'function to string' (without randomized uuid).
     //  This should work because two traits sharing the same name but being different IDs should be rare.
     //  In that rare case, we can probably live with being indeterministic.
 
-    create_ast(module, &transpiler_context, representations, runtime)
+    create_ast(module, &transpiler, representations, runtime, builtin_namespace)
 }
 
-pub fn create_ast(module: &Module, transpiler_context: &TranspilerContext, mut representations: Representations, runtime: &Runtime) -> Result<Box<ast::Module>, InterpreterError> {
-    let mut global_namespace = builtins::register(runtime, &mut representations);
+pub fn create_ast(module: &Module, transpiler_context: &Transpiler, mut representations: Representations, runtime: &Runtime, mut builtin_namespace: namespaces::Level) -> Result<Box<ast::Module>, InterpreterError> {
     let builtin_structs: HashSet<_> = representations.type_ids.keys().map(Clone::clone).collect();
-    let mut file_namespace = global_namespace.add_sublevel();
+    let mut file_namespace = builtin_namespace.add_sublevel();
     let mut object_namespace = namespaces::Level::new();  // TODO Actual keywords can't be in object namespace either
 
     let reverse_mapped_calls = transpiler_context.monomorphizer.get_mono_call_to_original_call();
@@ -180,7 +95,7 @@ pub fn create_ast(module: &Module, transpiler_context: &TranspilerContext, mut r
         }
     }
 
-    let mut names = global_namespace.map_names();
+    let mut names = builtin_namespace.map_names();
     names.extend(object_namespace.map_names());
 
     if module.main_functions.len() > 1 {
