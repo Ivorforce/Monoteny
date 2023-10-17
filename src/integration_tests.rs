30faf9646e74cc2301472b3d41383d6a2e5c8b5f
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2023-10-17 20:35:50 +0200

    Fix integration tests. run() fails because the interpreter is broken for non trivial cases still.

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -1,83 +1,89 @@
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+    use guard::guard;
     use itertools::Itertools;
     use crate::{interpreter, parser, transpiler};
-    use crate::interpreter::{common, Runtime};
+    use crate::error::RResult;
+    use crate::interpreter::Runtime;
     use crate::parser::ast::*;
-    use crate::program::functions::ParameterKey;
+    use crate::program::module::module_name;
+    use crate::transpiler::{Config, LanguageContext};
 
-    /// This tests the transpiler, interpreter and function calls.
-    #[test]
-    fn hello_world() {
-        let parsed = parser::parse_program(&"
-def @main :: {
+    const SIMPLE_PROGRAM: &str = "
+use!(module!(\"common\"));
+
+def main! :: {
     write_line(\"Hello World!\");
 };
 
-def @transpile :: {
+def transpile! :: {
     transpiler.add(main);
 };
-".to_string())?;
-        assert_eq!(parsed.global_statements.len(), 2);
-        assert!(parsed.global_statements[0].as_ref() == &GlobalStatement::FunctionDeclaration(Box::new(
-            Function {
-                target_type: None,
-                identifier: "main".to_string(),
-                parameters: vec![],
-                decorators: vec!["main".to_string()],
-                body: Some(Expression::from(vec![Box::new(Term::Block(vec![Box::new(Statement::Expression(Expression::from(vec![
-                    Box::new(Term::Identifier("write_line".to_string())),
-                    Box::new(Term::Struct(vec![
-                        StructArgument {
-                            key: ParameterKey::Positional,
-                            value: Expression::from(vec![Box::new(Term::StringLiteral(vec![StringPart::Literal("Hello World!".to_string())]))]),
-                            type_declaration: None,
-                        }
-                    ])),
-                ])))]))])),
-                return_type: None,
-            }
-        )));
+";
 
-        let mut runtime = Runtime::new();
+    #[test]
+    fn parse() -> RResult<()> {
+        let (parsed, errors) = parser::parse_program(SIMPLE_PROGRAM)?;
+        assert!(errors.is_empty());
 
-        let module = runtime.load_ast(&parsed)?;
+        assert_eq!(parsed.global_statements.len(), 3);
 
-        assert_eq!(module.function_implementations.len(), 2);
-        let ptr = module.function_pointers.values().filter(|ptr| &ptr.name == "main").exactly_one().unwrap();
-        let implementation = &module.function_implementations[&ptr.target];
-        assert_eq!(implementation.expression_forest.operations.len(), 2);
+        guard!(let Statement::FunctionDeclaration(function) = &parsed.global_statements[1].as_ref().value else {
+            panic!();
+        });
 
-        let python_ast = transpiler::python::transpile_module(&module, &mut runtime, true).expect("Python transpiler failed");
-        let python_string = python_ast.to_string();
-        assert!(python_string.contains("def main():"));
-        assert!(python_string.contains("print(\"Hello World!\")"));
-        assert!(python_string.contains("if __name__ == \"__main__\":"));
-        assert!(!python_string.contains("transpile"));
+        assert!(function.interface == FunctionInterface::Macro("main".to_string()));
+
+        Ok(())
+    }
+
+    /// This tests the transpiler, interpreter and function calls.
+    #[test]
+    fn run() -> RResult<()> {
+        let mut runtime = Runtime::new()?;
+        runtime.repository.add("common", PathBuf::from("monoteny"));
+
+        let module = runtime.load_source(SIMPLE_PROGRAM, module_name("main"))?;
+
+        assert_eq!(module.exposed_functions.len(), 2);
 
         // TODO Pass a pipe and monitor that "Hello World!" is printed.
-        interpreter::run::main(&module, &mut runtime).expect("Interpreter failed");
+        interpreter::run::main(&module, &mut runtime)?;
+
+        Ok(())
     }
 
     /// This tests generics, algebra and printing.
     #[test]
-    fn math() {
-        let parsed = parser::parse_program(&"
-def @main :: {
-    print(1 + 2 'Float32);
+    fn transpile_math() -> RResult<()> {
+        let (parsed, _) = parser::parse_program(&"
+use!(module!(\"common\"));
+
+def main! :: {
+    write_line(1 + 2 'Float32);
 };
 
-def @transpile :: {
+def transpile! :: {
     transpiler.add(main);
 };
 ".to_string())?;
 
-        let mut runtime = Runtime::new();
+        let mut runtime = Runtime::new()?;
+        runtime.repository.add("common", PathBuf::from("monoteny"));
 
-        let module = runtime.load_ast(&parsed)?;
+        let module = runtime.load_ast(&parsed, module_name("main"))?;
+        let mut context = transpiler::python::create_context(&runtime);
 
-        let python_ast = transpiler::python::transpile_module(&module, &mut runtime, true).expect("Python transpiler failed");
-        let python_string = python_ast.to_string();
+        let transpiler = interpreter::run::transpile(&module, &mut runtime)?;
+
+        let file_map = context.make_files("main", &runtime, transpiler, &Config {
+            should_constant_fold: true,
+            should_monomorphize: true,
+        })?;
+        let python_string = file_map["main.py"].to_string();
         assert!(python_string.contains("def main():"));
+
+        Ok(())
     }
 }
