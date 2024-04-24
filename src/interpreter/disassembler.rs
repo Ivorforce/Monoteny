cdd9eed84dd6ed8ce2402bbe45c644ee139d570d
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-04-24 17:10:13 +0200

    Disassemble args with padding.

--- src/interpreter/disassembler.rs
+++ src/interpreter/disassembler.rs
@@ -19,35 +19,37 @@ pub fn disassemble(chunk: &Chunk) {
 pub fn disassemble_one(ip: *const u8) -> usize {
     unsafe {
         let code = transmute::<u8, OpCode>(*ip);
+        // TODO Somehow, {:<20?} doesn't pad correctly.
+        print!("{:<15}", format!("{:?}", code));
+
         match code {
             OpCode::NEG | OpCode::ADD | OpCode::SUB | OpCode::MUL | OpCode::DIV |
             OpCode::EQ | OpCode::NEQ | OpCode::GR | OpCode::GR_EQ  | OpCode::LE  | OpCode::LE_EQ |
             OpCode::MOD | OpCode::EXP | OpCode::LOG | OpCode::PARSE | OpCode::TO_STRING => {
-                print!("{:?}\t{:?}", code, transmute::<u8, Primitive>(*ip.add(1)));
+                print!("\t{:?}", transmute::<u8, Primitive>(*ip.add(1)));
                 return 1 + 1;
             },
             OpCode::LOAD8 => {
-                print!("{:?}\t{:?}", code, *ip.add(1));
+                print!("\t{:?}", *ip.add(1));
                 return 1 + 1;
             }
             OpCode::LOAD16 => {
-                print!("{:?}\t{:?}", code, read_unaligned(ip.add(1) as *mut u16));
+                print!("\t{:?}", read_unaligned(ip.add(1) as *mut u16));
                 return 1 + 2;
             }
             OpCode::LOAD32 | OpCode::LOAD_LOCAL | OpCode::STORE_LOCAL | OpCode::LOAD_CONSTANT => {
-                print!("{:?}\t{:?}", code, read_unaligned(ip.add(1) as *mut u32));
+                print!("\t{:?}", read_unaligned(ip.add(1) as *mut u32));
                 return 1 + 4;
             }
             OpCode::LOAD64 => {
-                print!("{:?}\t{:?}", code, read_unaligned(ip.add(1) as *mut u64));
+                print!("\t{:?}", read_unaligned(ip.add(1) as *mut u64));
                 return 1 + 8;
             }
             OpCode::LOAD128 => {
-                print!("{:?}\t{:?}", code, read_unaligned(ip.add(1) as *mut u128));
+                print!("\t{:?}", read_unaligned(ip.add(1) as *mut u128));
                 return 1 + 16;
             }
             OpCode::NOOP | OpCode::PANIC | OpCode::RETURN | OpCode::TRANSPILE_ADD | OpCode::AND | OpCode::OR | OpCode::POP64 | OpCode::POP128 | OpCode::PRINT | OpCode::NOT | OpCode::ADD_STRING => {
-                print!("{:?}", code);
                 return 1;
             },
         }
