b630a338c0e63cb43fbbca0e62a7e0a9353da962
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-04-29 13:55:57 +0200

    Improve error message when a token was encountered without expectations.

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -19,12 +19,10 @@ pub fn map_parse_error(e: &ParseError<usize, Token, Error>) -> RuntimeError {
             RuntimeError::error("Invalid token.").in_range(*location..*location)
         },
         ParseError::UnrecognizedEof { location, expected } => {
-            RuntimeError::error("File ended unexpectedly.").in_range(*location..*location)
-                .with_note(make_expected_note(expected))
+            add_expected_note(RuntimeError::error("File ended unexpectedly.").in_range(*location..*location), expected)
         }
         ParseError::UnrecognizedToken { token: (start, token, end), expected } => {
-            RuntimeError::error("Unexpected token.").in_range(*start..*end)
-                .with_note(make_expected_note(expected))
+            add_expected_note(RuntimeError::error("Unexpected token.").in_range(*start..*end), expected)
         }
         ParseError::ExtraToken { token: (start, token, end) } => {
             RuntimeError::error("Extraneous token.").in_range(*start..*end)
@@ -46,10 +44,10 @@ fn unquote(value: &str) -> &str {
     chars.as_str()
 }
 
-fn make_expected_note(expected: &Vec<String>) -> RuntimeError {
+fn add_expected_note(error: RuntimeError, expected: &Vec<String>) -> RuntimeError {
     match &expected[..] {
-        [] => RuntimeError::note("Internal Error (expected nothing)."),
-        [one] => RuntimeError::note(format!("Expected: {}", unquote(one)).as_str()),
-        expected => RuntimeError::note(format!("Expected one of: {}", expected.iter().map(|s| unquote(s)).join(" ")).as_str()),
+        [] => error,
+        [one] => error.with_note(RuntimeError::note(format!("Expected: {}", unquote(one)).as_str())),
+        expected => error.with_note(RuntimeError::note(format!("Expected one of: {}", expected.iter().map(|s| unquote(s)).join(" ")).as_str())),
     }
 }
